27a1a228ff9c846b057546b9edf57417a5fb23b3
Diego Dal Bosco <diegodalbo97@gmail.com>
2015-12-12 09:48:08 +0100

    Aggiustata versione
    Istruzione sbagliata che dava sempre errore

--- orario.py
+++ orario.py
@@ -32,12 +32,15 @@ def log(msg):
 
 def insertVersione(filename):
 	'''
+
 	'''
-	rs = cursor.execute("SELECT MAX(versione) AS versione FROM GPUVersione").fetchone()
 	try:
-		field_versione = rs.versione + 1
+		cursor.execute("SELECT MAX(versione) AS versione FROM GPUVersione")
+		rs = cursor.fetchone()
+		field_versione = rs[0] + 1
 	except:
 		field_versione = 1
+
 	field_data = time.ctime(os.path.getmtime(filename))
 	query = "INSERT INTO GPUVersione (versione, dataexport) VALUES ("+str(field_versione)+",'"+str(field_data)+"')"
 
@@ -47,15 +50,16 @@ def insertVersione(filename):
 	if (boolSQL):
 		cursor.execute(query)
 	
-	return(field_versione)
+	return field_versione
 	
 	
 def insertTable(filename, name, versione):
 	'''
+
 	'''
 	lunghezza = 0
 
-	cursor.execute("DELETE FROM "+name)
+	#cursor.execute("DELETE FROM "+name)
 	
 	with open(filename, 'r') as f:
 		first_line = f.readline()
@@ -68,7 +72,7 @@ def insertTable(filename, name, versione):
 		parziale+=("`Column "+str(i)+"`,")
         
 	parziale = parziale[:-1]
-	parziale+=") VALUES ("
+	parziale+=",versione) VALUES ("
 
 	with open(filename, 'r') as f:
 		for line in f:
@@ -94,32 +98,34 @@ if (__name__ == "__main__"):
 	print("Starting: SQL is ", boolSQL )
 	if (server == 'MYSQL'):
 		#-- Connessione MYSQL
-		connection = mysql.connector.connect(user='root',password='',host='localhost',database='orario')	
+		connection = mysql.connector.connect(user='root',password='',host='',database='')	
+		#connection = pyodbc.connect("DRIVER={MySQL ODBC 3.51 Driver}; SERVER=localhost;DATABASE=orario; UID=root; PASSWORD=root$$2014;")
 
 	if (server == 'SYS01'):
 		#-- Connessione MSSQL
-		connection = pyodbc.connect('DRIVER={SQL Server};SERVER=edu-sys01\marconi;DATABASE=;UID=;PWD=')
+		connection = pyodbc.connect('DRIVER={SQL Server};SERVER=;DATABASE=;UID=;PWD=')
 
 	if (server == 'GARUDA63'):
 		#-- Connessione MSSQL
-		connection = pyodbc.connect('DRIVER={SQL Server};SERVER=;DATABASE=;UID=;PWD=')
+		connection = pyodbc.connect('DRIVER={SQL Server};SERVER=;DATABASE=;UID=sigma;PWD=')
 
 	#-- OPEN CURSOR
-	cursor = connection.cursor()
+	cursor = connection.cursor(buffered=True)
 	
-	boolFirstfile = True
+	boolFirstfile = False
 	
 	with open("conf.conf", 'r') as f:
 		for line in f:
-			line = line.replace('\n','').split(',')
+			line = line.replace('\n','').replace('\r','').split(',')
 			if (line[0][0] != '#'):
 				print (line)
 				log("file: "+line[0]+" table: "+line[1])
+				print("file: "+line[0]+" table: "+line[1])
 				if (boolFirstfile == False):
 					field_versione = insertVersione(line[0])
 					boolFirstfile = True
 
-				field_versione = 1
+				#field_versione = 1
 				insertTable(line[0], line[1], field_versione)
 	
 	
